fa11e155b13aaeef96af9465e1c911e357c93229
sindrekodehode <131758625+sindrekodehode@users.noreply.github.com>
2023-08-09 11:40:13 +0200

    Initial upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const clearButtonEl = document.getElementById("clear-button");
+const shoppingListEl = document.getElementById("shopping-list");
+const shoppingList = [];
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+  if (inputValue.trim() === "") {
+    console.log("Please enter a value!");
+    return;
+  }
+  appendItemSound();
+
+  shoppingList.push(inputValue);
+  localStorage.clear();
+
+  localStorage.setItem("list", JSON.stringify(shoppingList));
+
+  clearShoppingListEl();
+  renderShoppingList();
+
+  clearInputFieldEl();
+});
+
+clearButtonEl.addEventListener("click", function () {
+  removeItemSound();
+  localStorage.clear();
+  clearShoppingListEl();
+});
+
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = "";
+}
+
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
+}
+
+function renderShoppingList() {
+  let renderList = JSON.parse(localStorage.getItem("list"));
+  for (let item of renderList) {
+    let newEl = document.createElement("li");
+    newEl.innerText = item;
+    shoppingListEl.appendChild(newEl);
+  }
+}
+
+function appendItemSound() {
+  const audio1 = document.querySelector("#add-item-sound");
+  audio1.play();
+}
+
+function removeItemSound() {
+  const audio2 = document.querySelector("#remove-item-sound");
+  audio2.play();
+}
